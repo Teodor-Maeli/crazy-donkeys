a927be26ddbf197f5f7b0d3b47b543fba541fe63
teodor-maeli <teodor.maeli@brainstarstech.com>
2023-01-02 11:49:31 +0200

    slider wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  slider: HTMLDivElement = document.createElement('div');
+  percentage: number = 0;
+  translate: string = '-translate-x-[0%]';
+  interval!: NodeJS.Timer;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.slider = document.getElementById('slider') as HTMLDivElement;
+    this.interval = setInterval(() => {
+      this.slideNext();
+    }, 5000);
   }
 
+  slideNext(): void {
+    if (this.percentage === 75) {
+      this.percentage = -25;
+      this.slideNext();
+      return;
+    }
+    this.percentage = this.percentage + 25;
+    this.translate = '-translate-x-[' + this.percentage + '%]';
+
+    console.log(this.slider); 
+  }
+
+  slidePrev(): void {
+    if (this.percentage === 0) {
+      return;
+    }
+    this.percentage = this.percentage - 25;
+    this.translate = '-translate-x-[' + this.percentage + '%]';
+  }
+
+  onHoverStopSlider(): void {
+    clearInterval(this.interval);
+  }
 }
